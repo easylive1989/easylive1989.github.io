4f5bb3a9427a06679b9e386d0a557c58ec049f36
Paul <easylive1989@gmail.com>
2023-04-08 13:33:47 +0800

    update

--- paul-projects/src/App.js
+++ paul-projects/src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const projectsMap = {
+  "NS-SHAFT": "https://playernsshaft.web.app/#/",
+  "breakout": "https://easylive1989.github.io/flutter_breakout"
+}
 
 function App() {
+
+  const [key, setKey] = useState("NS-SHAFT")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header d-flex align-items-center">
+        <div className="d-flex align-items-start mb-4">
+          {
+            Object.keys(projectsMap).map((key) => {
+              return <button type="button" className="btn btn-primary ms-2 me-2" onClick={() => setKey(key)}>{key}</button>
+            })
+          }
+        </div>
+        <iframe name="iframe" src={projectsMap[key]}></iframe>
       </header>
     </div>
   );
